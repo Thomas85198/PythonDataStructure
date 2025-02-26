89307b8f001df281acc25475e691730a9612085b
luthomas <Thomas.Lu@garmin.com>
2025-02-26 11:48:18 +0800

    space_complexity.py

--- 複雜度分析/space_complexity.py
+++ 複雜度分析/space_complexity.py
@@ -42,3 +42,20 @@ def linear_recur(n: int):
 def quadratic(n: int):
     """ 平方階 """
     num_matrix = [[0] * n for _ in range(n)]
+
+
+def quadratic_recur(n: int) -> int:
+    """ 平方階 (遞迴實現) """
+    if n <= 0:
+        return 0
+    nums = [0] * n
+    return quadratic_recur(n - 1)
+
+
+def build_tree(n: int) -> TreeNode | None:
+    if n == 0:
+        return None
+    root = TreeNode(0)
+    root.left = build_tree(n - 1)
+    root.right = build_tree(n - 1)
+    return root
